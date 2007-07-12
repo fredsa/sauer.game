3c85083e231e53e061fd4e445e285dc00cc59d34
Fred Sauer <fredsa@gmail.com>
2007-07-12 21:53:10 +0000

    separate demo module

--- src/com/allen_sauer/gwt/game/client/Game.java
+++ src/com/allen_sauer/gwt/game/client/Game.java
@@ -1,24 +1,19 @@
 package com.allen_sauer.gwt.game.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.WindowResizeListener;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
 
-import com.allen_sauer.gwt.log.client.Log;
-
-public class Game implements EntryPoint {
-  private Image backgroundImage;
-  private int clientWidth;
-  private int clientHeight;
-
+public abstract class Game implements EntryPoint {
   public void onModuleLoad() {
-    backgroundImage = new Image("images/nebula_13-fudged.jpg");
-    backgroundImage.addStyleName("backgroundImage");
-    RootPanel.get().add(backgroundImage);
-    initClientSize();
-
     Window.addWindowResizeListener(new WindowResizeListener() {
       public void onWindowResized(int width, int height) {
         //Log.debug("resize event");
@@ -26,23 +21,12 @@ public class Game implements EntryPoint {
       }
     });
 
-    handleClientResized(clientWidth, clientHeight);
+    handleClientResized(Window.getClientWidth(), Window.getClientHeight());
+    assert Window.getClientWidth() != 0;
+    assert Window.getClientHeight() != 0;
   }
 
-  private void initClientSize() {
-    clientWidth = Window.getClientWidth();
-    clientHeight = Window.getClientHeight();
-    if (clientWidth == 0 || clientHeight == 0) {
-      //      Log.warn("Window.getClientWidth()=" + clientWidth + ", Window.getClientHeight()=" + clientHeight);
-      clientWidth = 100;
-      clientHeight = 100;
-    }
-  }
-
-  private void handleClientResized(int width, int height) {
-    //    Log.debug("handleClientResized(" + width + ", " + height + ")");
-    clientWidth = width;
-    clientHeight = height;
-    backgroundImage.setPixelSize(width, height);
+  protected void handleClientResized(int clientWidth, int clientHeight) {
+    //    Log.debug("handleClientResized(" + clientWidth + ", " + clientHeight + ")");
   }
 }
