2e41c22d98e7b8eac878e453ae03bbf6b5b64767
Fred Sauer <fredsa@gmail.com>
2007-07-15 14:54:52 +0000

    moved ~ directory

--- Game-compile.cmd
+++ Game-compile.cmd
@@ -1,1 +1,1 @@
-@java -cp "%~dp0\src;%~dp0\demo;%~dp0\bin;C:/fred/java/lib/gwt-log.jar;C:/home/fsauer/gwt/trunk/user/src;C:/home/fsauer/gwt/trunk/user/super;C:/home/fsauer/gwt/trunk/build/staging/gwt-windows-0.0.0/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" %* com.allen_sauer.gwt.game.Game
+@java -cp "%~dp0\src;%~dp0\demo;%~dp0\bin;C:/fred/java/lib/gwt-log.jar;C:/fred/gwt/trunk/user/src;C:/fred/gwt/trunk/user/super;C:/fred/gwt/trunk/build/staging/gwt-windows-0.0.0/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" %* com.allen_sauer.gwt.game.Game
