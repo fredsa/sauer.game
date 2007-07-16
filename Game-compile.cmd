e86d2787eb09507b7bc5175791796f36bd1cb1b8
Fred Sauer <fredsa@gmail.com>
2007-07-16 15:06:38 +0000

    shell cleanup

--- Game-compile.cmd
+++ Game-compile.cmd
@@ -1,1 +1,1 @@
-@java -cp "%~dp0\src;%~dp0\demo;%~dp0\bin;C:/fred/java/lib/gwt-log.jar;C:/fred/gwt/trunk/user/src;C:/fred/gwt/trunk/user/super;C:/fred/gwt/trunk/build/staging/gwt-windows-0.0.0/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" %* com.allen_sauer.gwt.game.Game
+@java -cp "%~dp0\src;%~dp0\demo;%~dp0\bin;%GWT_LIBS%/gwt-log.jar;%GWT_TRUNK%/user/src;%GWT_WINDOWS%/gwt-user.jar;%GWT_WINDOWS%/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" %* com.allen_sauer.gwt.game.space.Space
