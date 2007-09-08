1d4600bd9cdf52ff77f66b30ded46293372f0f6d
Fred Sauer <fredsa@gmail.com>
2007-09-08 22:33:28 +0000

    Better uncaught exception handler to capture initialization problems

--- demo/com/allen_sauer/gwt/game/space/client/Space.java
+++ demo/com/allen_sauer/gwt/game/space/client/Space.java
@@ -2,14 +2,26 @@ package com.allen_sauer.gwt.game.space.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
 
 import com.allen_sauer.gwt.game.client.engine.Engine;
 import com.allen_sauer.gwt.log.client.LogUncaughtExceptionHandler;
 
 public class Space implements EntryPoint {
   public void onModuleLoad() {
+    // set uncaught exception handler
     GWT.setUncaughtExceptionHandler(new LogUncaughtExceptionHandler());
 
+    // use deferred command to catch initialization exceptions
+    DeferredCommand.addCommand(new Command() {
+      public void execute() {
+        onModuleLoad2();
+      }
+    });
+  }
+
+  public void onModuleLoad2() {
     //    Log.moveTo(0, 110);
     //    Log.setPixelSize(800, 300);
     SpaceGame game = new SpaceGame();
