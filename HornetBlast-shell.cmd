cb470d3c9cd938dd46fc43e720cfd3d797273a93
Fred Sauer <fredsa@gmail.com>
2007-12-12 05:03:16 +0000

    reorder classpath, add GWT15_TRUNK/user/src as user-src15 remove Voices project from launches compile with gwt-voices-0.0.0.jar instead of gwt-voices.jar Voices -> gwt-voices

--- HornetBlast-shell.cmd
+++ HornetBlast-shell.cmd
@@ -1,1 +1,1 @@
-java -cp "%~dp0\src;%~dp0\hornetblast;%GWT_LIBS%/gwt-log/gwt-log.jar;%GWT_LIBS%/gwt-voices/gwt-voices.jar;%GWT_PLATFORM%/gwt-user.jar;%GWT_PLATFORM%/gwt-dev-windows.jar" com.google.gwt.dev.GWTShell -out "%~dp0\www" %* com.allen_sauer.gwt.game.hornetblast.HornetBlast/HornetBlast.html
+java -cp "%~dp0\src;%~dp0\hornetblast;%GWT_LIBS%/gwt-log/gwt-log.jar;%GWT_LIBS%/gwt-voices/gwt-voices-0.0.0.jar;%GWT_PLATFORM%/gwt-user.jar;%GWT_PLATFORM%/gwt-dev-windows.jar" com.google.gwt.dev.GWTShell -out "%~dp0\www" %* com.allen_sauer.gwt.game.hornetblast.HornetBlast/HornetBlast.html
