7450eae2f5dfa8df6370a72d338f7c9f2ec7b1d2
Fred Sauer <fredsa@gmail.com>
2007-12-13 21:31:15 +0000

    Missouri State jar

--- hornetblast/com/allen_sauer/gwt/game/hornetblast/client/HornetGameOverPanel.java
+++ hornetblast/com/allen_sauer/gwt/game/hornetblast/client/HornetGameOverPanel.java
@@ -9,11 +9,7 @@ import com.allen_sauer.gwt.game.client.Game;
 import com.allen_sauer.gwt.game.hornetblast.client.ui.HornetLabel;
 
 public class HornetGameOverPanel extends HornetOverlayPanel {
-  private final Game game;
-
   public HornetGameOverPanel(Game game) {
-    this.game = game;
-
     HornetLabel gameOver = new HornetLabel("GAME OVER");
     gameOver.addStyleName("hornet-game-state-label");
     add(gameOver);
