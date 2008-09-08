9f821a5f87517c344a59d230781ea906d7d7c968
Fred Sauer <fredsa@gmail.com>
2008-09-08 04:08:08 +0000

    player bullets are time throttled rather than frame throttled

--- src/com/allen_sauer/gwt/game/client/generator/PlayerBulletGenerator.java
+++ src/com/allen_sauer/gwt/game/client/generator/PlayerBulletGenerator.java
@@ -7,10 +7,10 @@ import com.allen_sauer.gwt.game.client.Game;
 import com.allen_sauer.gwt.game.client.sprite.SpritePool;
 
 public class PlayerBulletGenerator implements Generator {
-  private static final int FRAMES_BETWEEN_BULLETS = 7;
-  private int frameCounter;
+  private static final double MILLIS_BETWEEN_BULLETS = 150;
   private final Game game;
   private boolean markedForRemoval = false;
+  private int milliCounter;
   private final SpritePool spritePool;
 
   public PlayerBulletGenerator(Game game, SpritePool spritePool) {
@@ -20,16 +20,15 @@ public class PlayerBulletGenerator implements Generator {
   }
 
   public void doFirstFrame() {
-    frameCounter = 0;
+    milliCounter = 0;
   }
 
   public boolean doFrame(double millis) {
-    double factor = millis / 20;
-
-    if (++frameCounter > factor * FRAMES_BETWEEN_BULLETS && !spritePool.exhausted()
+    milliCounter += millis;
+    if (milliCounter > MILLIS_BETWEEN_BULLETS && !spritePool.exhausted()
         && (game.input.isKeyDown(' ') || game.input.getClick())) {
       spritePool.create();
-      frameCounter = 0;
+      milliCounter = 0;
     }
     if (markedForRemoval) {
       markedForRemoval = false;
