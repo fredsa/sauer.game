57ff925516764e00c2c5bf001f134f612d2c1c32
Fred Sauer <fredsa@gmail.com>
2009-09-25 05:32:47 +0000

    remove obsolete scripts

--- Space-shell.cmd
+++
@@ -1,1 +0,0 @@
-java -cp "%~dp0\src;%~dp0\space;%GWT_LIBS%/gwt-log/gwt-log-0.0.0.jar;%GWT_LIBS%/gwt-voices/gwt-voices-0.0.0.jar;%GWT_TRUNK%/user/src;%GWT15_HOME%/gwt-user.jar;%GWT15_HOME%/gwt-dev-windows.jar" com.google.gwt.dev.GWTShell -out "%~dp0\www" %* com.allen_sauer.gwt.game.space.Space/Space.html
